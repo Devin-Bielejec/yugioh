02abda36c7780fdefbb57221d6d5c13079f1feee
devin-bielejec <devin.bieleje@gmail.com>
2019-08-15 13:53:54 -0400

    Decided basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  useEffect( () => {
+    const getData = () => {
+      axios
+      .get("https://db.ygoprodeck.com/api/v5/cardinfo.php")
+      .then(res => console.log(res));
+    }
+    getData();
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title />
+        <AttackingCard />
+        <HOTCButton />
       </header>
     </div>
   );
